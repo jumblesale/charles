4ef987365a1fdd7d5f98b261e4a1b5516498f0d8
Rob Sollars <rob@robsollars.co.uk>
2014-05-10 18:36:08 +0100

    Basic webserver provides random image from specified directory

---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var opts = require('nomnom')
+	.option('dir', {
+		abbr: 'd',
+		help: 'The directory containing images'
+	})
+	.parse(),
+
+	fs = require('fs'),
+	http = require('http'),
+	path = require('path'),
+	_ = require('underscore'),
+
+	formats = ['jpg', 'jpeg', 'png'],
+
+	image;
+
+console.log('Reading files from %s ...', opts.dir);
+
+randomImage = function(imageDir) {
+	var images = _.filter(fs.readdirSync(imageDir), function(file) {
+			var pattern = /^.+\.(.+)$/,
+				matches = pattern.exec(file),
+				extension;
+			if(!matches) {
+				return false;
+			}
+			extension = matches[1];
+			return _.contains(formats, matches[1]);
+		}),
+
+		image = (function(images) {
+			var length = images.length,
+				rand = Math.floor((Math.random() * length) + 1);
+			return images[rand];
+		})(images);
+
+	console.log('Displaying image: %s', image);
+
+	return fs.readFileSync(path.join(imageDir, image));
+};
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'image/JPEG'});
+	res.end(randomImage(opts.dir));
+}).listen(16217, '127.0.0.1');
